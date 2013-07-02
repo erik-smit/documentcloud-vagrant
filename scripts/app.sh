a6fbf9f9f507a750f2ae34a68e14d497c83f5963
Gonzalo Matheu <gmatheu@thoughtworks.com>
2013-07-02 12:20:10 -0300

    Using DCLOUD_HOME, Removing unnecesary 'sudo' calls

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,18 +1,20 @@
 # Ensure nginx is installed
-test -e /usr/local/nginx || {sudo gem install passenger; sudo /usr/local/bin/passenger-install-nginx-module --auto --auto-download \
-    --prefix /usr/local/nginx --extra-configure-flags='--with-http_gzip_static_module --with-http_ssl_module --with-http_stub_status_module'}
+test $DCLOUD_HOME || { echo "DCLOUD_HOME has to be set" >&2; exit 1; }
+
+test -e /usr/local/nginx || { gem install passenger ; /usr/local/bin/passenger-install-nginx-module --auto --auto-download \
+    --prefix /usr/local/nginx --extra-configure-flags='--with-http_gzip_static_module --with-http_ssl_module --with-http_stub_status_module'; }
 
 test -e /usr/local/nginx || { echo "nginx not properly installed" >&2; exit 1; }
 
 LINE='export PATH=$PATH:/usr/local/nginx/sbin'
 grep -q "$LINE" .bashrc 2>/dev/null || echo "$LINE" >> .bashrc
 
-sudo mkdir -p /usr/local/nginx/conf/sites-enabled /var/log/nginx/
-sudo ruby /home/vagrant/scripts/configure_nginx.rb
+mkdir -p /usr/local/nginx/conf/sites-enabled /var/log/nginx/
+ruby /vagrant/scripts/configure_nginx.rb
 
-cd /home/vagrant/documentcloud
+cd $DCLOUD_HOME
 test -e secrets/keys || { cp -r config/server/keys secrets/ ; }
 
-sudo cp config/server/nginx/nginx.init /etc/init.d/nginx
-sudo update-rc.d nginx defaults
-sudo /etc/init.d/nginx start
+cp config/server/nginx/nginx.init /etc/init.d/nginx
+update-rc.d nginx defaults
+service nginx start
